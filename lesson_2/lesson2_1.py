15a088726f549df4e1633af9beb721dc4d4ea7ef
VR <rozhkovvv@gmail.com>
2023-02-21 21:39:38 +0300

    lesson 2

---
+++ lesson_2/lesson2_1.py
@@ -0,0 +1,15 @@
+
+
+
+number = int(input("Введите число: "))
+result = 0
+b = 0
+for i in range(number):
+    temp = int(input("Введите температуру"))
+    if temp >= 0:
+        result += 1
+    else:
+        if result > b:
+            b = result
+        result = 0
+print(b)
